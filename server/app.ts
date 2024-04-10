b2c52393b2dd5e8dacaddde4248655809c6ca17a
md tanwir <tanw9004167@gmail.com>
2024-04-11 02:02:39 +0530

    #

--- server/app.ts
+++ server/app.ts
@@ -22,14 +22,19 @@ app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 
 // cors => cross origin resource sharing
-app.use(
-  cors({
-    // origin: ['http://localhost:3000'],
-     origin: ["https://tech-edu-coder-client.vercel.app/"],
-     methods: ['POST', 'GET', 'PUT', 'PATCH', 'DELETE', 'HEAD'],
-     credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     // origin: ['http://localhost:3000'],
+//      origin: ["https://tech-edu-coder-client.vercel.app/"],
+//      methods: ['POST', 'GET', 'PUT', 'PATCH', 'DELETE', 'HEAD'],
+//      credentials: true,
+//   })
+// );
+app.use(cors({
+  origin: ["https://tech-edu-coder-client.vercel.app"],
+  methods: ['POST', 'GET', 'PUT', 'PATCH', 'DELETE', 'HEAD'],
+  credentials: true,
+}));
 
 
 // api requests limit
