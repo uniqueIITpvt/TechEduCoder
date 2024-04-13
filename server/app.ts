136c843805671c30dc17d0fa823ff6c7a20690b5
md tanwir <tanw9004167@gmail.com>
2024-04-13 10:50:54 +0530

    #

--- server/app.ts
+++ server/app.ts
@@ -69,7 +69,7 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-// unknown route
+
 app.all('*', (req: Request, res: Response, next: NextFunction) => {
   const err = new Error(`Route ${req.originalUrl} not found`) as any;
   err.statusCode = 404;
