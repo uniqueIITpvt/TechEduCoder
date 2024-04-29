ac926340a90e87de4887810be9a509d01c8d0958
md tanwir <tanw9004167@gmail.com>
2024-04-29 15:13:17 +0530

    #

--- client/app/components/Route/Hero.tsx
+++ client/app/components/Route/Hero.tsx
@@ -6,6 +6,7 @@ import React, { FC } from "react";
 import logo3 from "../../../public/1.jpg";
 import logo1 from "../../../public/2.jpg";
 import logo2 from "../../../public/3.jpg";
+import logo4 from "../../../public/222222222222222.jpg";
 import Link from "next/link";
 
 type Props = {};
@@ -14,7 +15,7 @@ type Props = {};
 const Hero: FC<Props> = (props) => {
   return (
     <>
-      <div className="w-full h-auto">
+      <div className="w-full h-[70vh] md:h-auto">
         <div className="relative">
           <Carousel
             swipeable={false}
@@ -25,35 +26,39 @@ const Hero: FC<Props> = (props) => {
             showStatus={false}
             interval={3000}
             transitionTime={500}
+            className="h-[70vh] md:h-auto "
           >
             {/* Ensure your images are responsive */}
-            <Image src={logo1} alt="" layout="responsive" />
-            <Image src={logo3} alt="" layout="responsive" />
-            <Image src={logo2} alt="" layout="responsive" />
+            <Image src={logo1} alt="" className=" h-[70vh] object-cover" />
+            <Image src={logo3} alt="" className=" h-[70vh] object-cover" />
+            <Image src={logo2} alt="" className=" h-[70vh] object-cover" />
+            
           </Carousel>
 
-          <div className="absolute bottom-0 left-0 top-0 h-full w-[60%] bg-gradient-to-r from-sky-500 md:pt-10 pl-2 md:pl-0">
-            <h1 className="text-white text-[20px]  leading-7 md:leading-normal pl-8 md:pt-5 pt-2 font-[700] xl:pt-20 xl:pl-16  lg:text-[40px] lg:pl-10    md:text-[25px]  md:pl-4   sm:ml-[30px] sm:pl-[10px] sm:mb-[10px sm:font-[700]]   ">
-              Upgrade your learning <br /> Skills & Upgrade your life
+          <div className="absolute bottom-0 left-0 top-0 h-full md:w-[60%] w-[80%]  md:bg-gradient-to-r from-sky-500 md:pt-10 pl-2 md:pl-0">
+            <h1 className=" text-[#0a083b] md:text-white text-[28px] md:text-[28px] md:ml-10 pl-4  leading-9  font-bold lg:ml-[5rem] lg:text-[48px]   lg:leading-[56px] lg:font-extrabold font-sans lg:mt-10 opacity-90 mt-8  ">
+              Upgrade your learning Skills <br />& Upgrade your life
             </h1>
             {/* Hidden on mobile, visible on md screens and up */}
-            <p className="hidden  lg:block lg:pl-16 xl:pl-24  text-indigo-700 leading-7 pt-2  pl-2  text-[18px] md:text-[20px] md:hidden font-[700]">
+            <p className=" text-indigo-700 leading-[18px] pt-2  pl-4  text-[14px] font-[700] md:text-[17px] md:ml-9 md:font-[700] md:leading-[22px] lg:ml-20 lg:mt-6">
               Hand-picked Instructor and expertly crafted courses, designed for
               <br /> the modern students and entrepreneur.
             </p>
-            <div className="flex py-2 md:hidden pl-2 md:pl-10 lg:flex lg:mt-5 xl:pl-20">
-              <div className="p-2 md:p-3">
+            <div className="flex mt-6  font-poppins font-[600] items-center justify-between ml-5 w-full  md:justify-start md:px-6  lg:ml-16  ">
+              <div className="bg-gradient-to-r from-blue-500 to-[#521088] text-white rounded-lg  hover:bg-gradient-to-br p-3 text-sm transition-all ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 md:mr-6 lg:text-[16px] lg:p-4">
                 <Link
                   href={`/courses`}
-                  className="hidden md:hidden lg:block  bg-gradient-to-r from-blue-500 to-[#521088] items-center justify-center md:justify-start px-4 py-2 md:px-5 md:py-4 text-[17px] md:text-[18px] font-[500] text-center text-white rounded-lg hover:bg-gradient-to-br hover:text-white transition-all ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
+                  // className="  bg-gradient-to-r from-blue-500 to-[#521088]   text-center text-white rounded-lg hover:bg-gradient-to-br hover:text-white transition-all ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
+                  className=""
                 >
                   Browse Courses
                 </Link>
               </div>
-              <div className="p-2 md:p-3">
+              <div className="bg-gradient-to-r from-blue-500 to-[#521088] text-white rounded-lg  hover:bg-gradient-to-br p-3 text-sm transition-all ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 md:mr-6 lg:text-[16px] lg:p-4">
                 <Link
-                  href={`/`}
-                  className="hidden md:inline-flex bg-gradient-to-r from-blue-500 to-[#521088] items-center justify-center md:justify-start px-4 py-2 md:px-5 md:py-4 text-[17px] md:text-[18px] font-[500] text-center text-white rounded-lg hover:bg-gradient-to-br hover:text-white transition-all ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
+                  href={`/courses`}
+
+                  // className="bg-gradient-to-r from-blue-500 to-[#521088] items-center justify-center md:justify-start px-4 py-2 md:px-5 md:py-4 text-[17px] md:text-[18px] font-[500] text-center text-white rounded-lg hover:bg-gradient-to-br hover:text-white transition-all ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
                 >
                   Start Learning
                 </Link>
