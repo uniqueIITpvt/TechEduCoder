b409b03d8dd6c31c09b130652ad41fb038c9c8ae
md tanwir <tanw9004167@gmail.com>
2024-05-07 11:28:20 +0530

    error fix

--- client/app/components/eBooks/EbookDetailsPage.tsx
+++ client/app/components/eBooks/EbookDetailsPage.tsx
@@ -4,10 +4,6 @@ import Heading from "@/app/utils/Heading";
 import Header from "../Header";
 import Footer from "../Footer";
 import CourseDetails from "./EbookDetails";
-import {
-  useCreatePaymentIntentMutation,
-  useGetStripePublishablekeyQuery,
-} from "@/redux/features/orders/ordersApi";
 import { loadStripe } from "@stripe/stripe-js";
 import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
 import EbookDetails from "./EbookDetails";
@@ -22,31 +18,15 @@ const EbookDetailsPage = ({ id }: Props) => {
   const [open, setOpen] = useState(false);
   // const { data, isLoading } = useGetCourseDetailsQuery(id);
   // const { data: config } = useGetStripePublishablekeyQuery({});
-  const [createPaymentIntent, { data: paymentIntentData }] =
-    useCreatePaymentIntentMutation();
+
 
   const { data, isLoading } = useGetEbookQuery(id);
-  const { data: config } = useGetStripePublishablekeyQuery({});
+ 
   const { data: userData } = useLoadUserQuery(undefined, {});
   const [stripePromise, setStripePromise] = useState<any>(null);
   const [clientSecret, setClientSecret] = useState("");
 
-  useEffect(() => {
-    if (config) {
-      const publishablekey = config?.publishablekey;
-      setStripePromise(loadStripe(publishablekey));
-    }
-    if (data && userData?.user) {
-      const amount = Math.round(data.ebook.estimatedPrice * 100);
-      createPaymentIntent(amount);
-    }
-  }, [config, data, userData]);
 
-  useEffect(() => {
-    if (paymentIntentData) {
-      setClientSecret(paymentIntentData?.client_secret);
-    }
-  }, [paymentIntentData]);
  
 
   return (
--- client/app/eBook-access/[id]/page.tsx
+++ client/app/eBook-access/[id]/page.tsx
@@ -3,10 +3,6 @@ import React, { useEffect, useState } from "react";
 
 import Heading from "@/app/utils/Heading";
 
-import {
-  useCreatePaymentIntentMutation,
-  useGetStripePublishablekeyQuery,
-} from "@/redux/features/orders/ordersApi";
 import { loadStripe } from "@stripe/stripe-js";
 import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
 
@@ -30,33 +26,15 @@ const Page = ({params}:any) => {
   const [open, setOpen] = useState(false);
   const id = params.id
 
-  // const { data, isLoading } = useGetCourseDetailsQuery(id);
-  // const { data: config } = useGetStripePublishablekeyQuery({});
-  const [createPaymentIntent, { data: paymentIntentData }] =
-    useCreatePaymentIntentMutation();
+  
 
   const { data, isLoading } = useGetEbookQuery(id);
-  const { data: config } = useGetStripePublishablekeyQuery({});
+
   const { data: userData } = useLoadUserQuery(undefined, {});
   const [stripePromise, setStripePromise] = useState<any>(null);
   const [clientSecret, setClientSecret] = useState("");
 
-  useEffect(() => {
-    if (config) {
-      const publishablekey = config?.publishablekey;
-      setStripePromise(loadStripe(publishablekey));
-    }
-    if (data && userData?.user) {
-      const amount = Math.round(data.ebook.estimatedPrice * 100);
-      createPaymentIntent(amount);
-    }
-  }, [config, data, userData]);
 
-  useEffect(() => {
-    if (paymentIntentData) {
-      setClientSecret(paymentIntentData?.client_secret);
-    }
-  }, [paymentIntentData]);
  
  
 
